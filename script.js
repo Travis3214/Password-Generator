28bda8392ef45860b2dfe7a02c390aedfa3126f1
Travis <travis3214@yahoo.com>
2023-06-30 00:32:38 -0400

    fixed variables

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const NumArrary = [1,2,3,4,5,6,7,8,9,0]
-const special = ["!", "#", "$", "%", "&","*", "?", "@", "^"];
-const tallLetter = 
-const lowLetter = lowercase letters
+var length = 8;
+var choices = [];
+var numArrary = [1,2,3,4,5,6,7,8,9,0];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var special = ["!", "#", "$", "%", "&","*", "?", "@", "^"];
 
 
 // Assignment Code
