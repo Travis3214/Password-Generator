5001db849fc4bd8726f042a23b9395f95698c97a
Travis <travis3214@yahoo.com>
2023-06-30 14:14:27 -0400

    updated comments on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
-// Created arrays for characters in password
-// These are the options the user can pick from
+// Create arrays for characters user can pick from to include in password
+
+// This array will be for the length of the password
 var length = 8;
+// This array is to include each type of character(s) the user selects
 var choices = [];
+// Numbers array
 var numArrary = [1,2,3,4,5,6,7,8,9,0];
+// Capitol letters array
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// Lower case letters array
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// Special characters array
 var special = ["!", "#", "$", "%", "&","*", "?", "@", "^"];
 
 // Added a variable to the button for the user to click
 
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button. This responds to the users click
+// Added event listener to the generate button. This responds to the users click
 
 generateBtn.addEventListener("click", writePassword);
 
@@ -25,7 +31,7 @@ function generatePassword(){
     }
     return password
 }
-// This is a function to write the password after computer generates it
+// This is a function to write the password in the box after computer generates it
 
 function writePassword() {
     var answers = getPrompts();
@@ -45,6 +51,7 @@ function getPrompts(){
     choices = [];
     length = parseInt(prompt("How many characters would you like your password to contain?"));
     
+    // These prompts/confirms ask the user for their input on what they want to include in the password. 
     if(isNaN(length) || length < 8 || length > 128) {
       alert("Password must be between 8 - 128 characters long.");
       return false;
