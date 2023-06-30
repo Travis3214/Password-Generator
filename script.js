45229a73eadb5933a2864aab4210f1ac23b2cb32
Travis <travis3214@yahoo.com>
2023-06-30 00:12:31 -0400

    started making arrays and attempted to start function

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+const NumArrary = [1,2,3,4,5,6,7,8,9,0]
+const special = ["!", "#", "$", "%", "&","*", "?", "@", "^"];
+const tallLetter = 
+const lowLetter = lowercase letters
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+  // prompt user for password criteria
+  let length = window.prompt("How many characters would you like your password to contain?");
+  if (length < 8 || length > 128){
+    alert("Password must be between 8 and 128 characters");
+    if (length > 8 || length < 128){
+      confirm ("Would you like to include lowercase letters?");
+    }
+  }
 
 // Write password to the #password input
 function writePassword() {
