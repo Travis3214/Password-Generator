e5d2af46f3c5e7e589a0f5e93c0fa879c495ff83
Travis <travis3214@yahoo.com>
2023-06-30 14:04:54 -0400

    updated read me and fixed folders for css and js

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Password-Generator
+
+## Description
+In this assignment we were asked to create the JavaScript logic to generate a password. Not just any password though. We want to generate a password that allows the user to select if they want to include "uppercase letters", "lowercase letters", "special characters", or "numbers". We want to prompt the user when they click the button. The prompt will ask the user how many characters they want their password to be. Then confirm with the user which type of characters they will include in the generation. 
+
+## Installation
+N/A
+
+## Usage
+
+
+## License
+N/A
+
+## Screenshot
+![Password-Generator](./assets/images/_Users_travis_Documents_UCF_Homework_Password-Generator_index.html.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Created arrays for characters in password
+// These are the options the user can pick from
 var length = 8;
 var choices = [];
 var numArrary = [1,2,3,4,5,6,7,8,9,0];
@@ -6,9 +7,26 @@ var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var special = ["!", "#", "$", "%", "&","*", "?", "@", "^"];
 
+// Added a variable to the button for the user to click
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Add event listener to generate button. This responds to the users click
+
+generateBtn.addEventListener("click", writePassword);
+
+// This is a function to generate the password after receiving the user inputs
+
+function generatePassword(){
+    var password = "";
+    for(var index = 0; index < length; index++){
+      var randomize = Math.floor(Math.random() * choices.length);
+      password = password + choices[randomize];
+    }
+    return password
+}
+// This is a function to write the password after computer generates it
+
 function writePassword() {
     var answers = getPrompts();
     var passwordText = document.querySelector("#password");
@@ -21,24 +39,14 @@ function writePassword() {
   }
 }
 
-  // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-function generatePassword(){
-    var password = "";
-    for(var index = 0; index < length; index++){
-      var randomize = Math.floor(Math.random() * choices.length);
-      password = password + choices[randomize]
-    }
-    return password
-}
+// This function receives the users input and decides what characters should be used when generating the password
 
 function getPrompts(){
     choices = [];
     length = parseInt(prompt("How many characters would you like your password to contain?"));
     
-    if(isNan(length) || length < 8 || length > 128) {
-      alert("Must be a number between 8 - 128.");
+    if(isNaN(length) || length < 8 || length > 128) {
+      alert("Password must be between 8 - 128 characters long.");
       return false;
     }
 
